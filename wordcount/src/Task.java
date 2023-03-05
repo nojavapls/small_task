22b041ae4c20a1d9b9326c9b8c8be36ff98d6577
nojavapls <115868577+nojavapls@users.noreply.github.com>
2023-03-05 17:05:37 +0300

    Update Task.java

--- wordcount/src/Task.java
+++ wordcount/src/Task.java
@@ -13,8 +13,23 @@ public class Task {
         String sentence = scanner.nextLine();
         List<String> words = Arrays.asList(sentence.split(" "));
 
-        List<Long> result_int = words.stream().collect(groupingBy(identity(), counting())).entrySet().stream().sorted(Map.Entry.<String, Long> comparingByValue(reverseOrder()).thenComparing(Map.Entry.comparingByKey())).limit(10).map(Map.Entry::getValue).collect(toList());
-        List<String> result_words = words.stream().map(String::toLowerCase).collect(groupingBy(identity(), counting())).entrySet().stream().sorted(Map.Entry.<String, Long> comparingByValue(reverseOrder()).thenComparing(Map.Entry.comparingByKey())).limit(10).map(Map.Entry::getKey).collect(toList());
+        List<Long> result_int = words.stream()
+            .collect(groupingBy(identity(), counting()))
+            .entrySet().stream()
+            .sorted(Map.Entry.<String, Long> comparingByValue(reverseOrder())
+            .thenComparing(Map.Entry.comparingByKey()))
+            .limit(10)
+            .map(Map.Entry::getValue)
+            .collect(toList());
+        List<String> result_words = words.stream()
+            .map(String::toLowerCase)
+            .collect(groupingBy(identity(), counting()))
+            .entrySet()
+            .stream()
+            .sorted(Map.Entry.<String, Long> comparingByValue(reverseOrder())
+            .thenComparing(Map.Entry.comparingByKey()))
+            .limit(10).map(Map.Entry::getKey)
+            .collect(toList());
         System.out.println("TOP 10 WORDS SORTED BY OCCURANCY AND SORTED IN ALFAVIT PORYADOK");
         for (int i = 0; i < result_int.size(); i++) {
             System.out.println(result_int.get(i)+" "+ result_words.get(i));
